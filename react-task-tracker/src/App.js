728c1693652579d9d97f663db6331715414dd66c
Anh Nguyen <ba2nguye@uwaterloo.ca>
2021-06-19 02:52:50 -0400

    Added a close/add toggle

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -11,6 +13,12 @@ const App = () => {
         reminder: false,
     }
   ])
+  // Add Task
+  const addTask = (task) => {
+    const id= Math.floor(Math.random() * 10000) +1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -28,7 +36,9 @@ const App = () => {
 
   return (
     <div className='container'>
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} 
         onDelete={deleteTask}
